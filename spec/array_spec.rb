a17bc58e70ab318b91d41caf3515941ab08c4db8
iman4325 <imantanzeem@hotmail.com>
2018-07-10 20:52:43 +0000

    automatically backed up by learn

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -17,12 +17,6 @@ describe "change_rainbow_colors" do
     converted_rainbow.each { |c| 2.times { 45.times {print "\u2588".colorize(c)}; puts "" } }
     puts ""
     rainbow_colors = change_rainbow_colors
-    array = []
-    array[0]=red
-    array[1] = light_red
-    array[2]= light_yellow
-    array[3] = green
-    array[4]= blue 
     expect(rainbow_colors).to be_kind_of(Array), "Method must return an array"
     converted_rainbow = rainbow_colors.map {|color| color.to_sym}
     converted_rainbow.each { |c| 2.times { 45.times {print "\u2588".colorize(c)}; puts "" } }
